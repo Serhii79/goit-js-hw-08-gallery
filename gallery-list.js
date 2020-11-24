574e375fba4fa4303e77cf42157b8dd8e1b4c75e
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-24 22:41:42 +0200

    Create the set of the gallery

---
+++ gallery-list.js
@@ -0,0 +1,34 @@
+import arrayImages from './gallery-items.js';
+
+{/* <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+  >
+    <img
+      class="gallery__image"
+      src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
+      data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
+      alt="Tulips"
+    />
+  </a>
+</li> */}
+
+const galleryRef = document.querySelector('.gallery');
+
+const createLi = ({ preview, original, description }) => `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+
+const createArrayLi = arrayImages.map((item) => createLi(item));
+galleryRef.insertAdjacentHTML('beforeend', createArrayLi.join(''));
