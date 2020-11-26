ecfb5b4985e647e246a6ea4db2bfb030851dc785
Serhii Bondar <sergiibondar1979@gmail.com>
2020-11-27 01:33:13 +0200

    Create a modal test

--- gallery-list.js
+++ gallery-list.js
@@ -1,6 +1,8 @@
 import arrayImages from './gallery-items.js';
 
-const galleryRef = document.querySelector('.gallery');
+const galleryRef = document.querySelector('.js-gallery');
+const lightboxRef = document.querySelector('js-lightbox');
+const lightboxImageRef = document.querySelector('.lightbox__image');
 
 const createLi = ({ preview, original, description }) => `<li class="gallery__item">
   <a
@@ -23,6 +25,17 @@ const largeImage = document.querySelector('.gallery__image');
 
 galleryRef.addEventListener('click', event => {
   event.preventDefault();
-  console.log(event.target);
-  
+  // console.log(event.target);
+
+  if (event.target.nodeName != 'IMG') {
+    return;
+  }
+
+  const imageRef = event.target;
+  const largeImageUrl = imageRef.dataset.source;
+
+  console.log(largeImageUrl);
+  // lightboxRef.classList.add('.is-open');
+  // lightboxImageRef.scr = largeImageUrl;
+
 });
